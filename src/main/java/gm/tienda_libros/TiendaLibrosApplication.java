45c2cd643be730e3199e8b9ba67a42f0a8f9abc0
YISUSBG <jesusbersabegonzalez@gmail.com>
2025-08-07 20:20:26 +0200

    Second Commit clase Entidad Libro

--- src/main/java/gm/tienda_libros/TiendaLibrosApplication.java
+++ src/main/java/gm/tienda_libros/TiendaLibrosApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(TiendaLibrosApplication.class, args);
 	}
 
