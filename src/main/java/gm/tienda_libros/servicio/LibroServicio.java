c5b529a25aa5215f5954d07b31479998a0192655
YISUSBG <jesusbersabegonzalez@gmail.com>
2025-08-07 21:26:29 +0200

    Thirth Commit Capa Repositorio y Servicio

---
+++ src/main/java/gm/tienda_libros/servicio/LibroServicio.java
@@ -0,0 +1,34 @@
+package gm.tienda_libros.servicio;
+
+import gm.tienda_libros.modelo.Libro;
+import gm.tienda_libros.repositorio.LibroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class LibroServicio implements ILibroServicio{
+
+    @Autowired
+    private LibroRepositorio libroRepositorio;
+
+    @Override
+    public List<Libro> listarLibros() {
+        return libroRepositorio.findAll();
+    }
+
+    @Override
+    public Libro buscarLibroPorId(Integer idLibro) {
+        Libro libro = libroRepositorio.findById(idLibro).orElse(null);
+        return libro;
+    }
+
+    @Override
+    public void guardarLibro(Libro libro) {
+        libroRepositorio.save(libro);
+    }
+
+    @Override
+    public void eliminarLibro(Libro libro) {
+        libroRepositorio.delete(libro);
+    }
+}
